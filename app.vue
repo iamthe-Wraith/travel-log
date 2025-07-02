ad42ceadd28ceb6a8b5ec8a7f1cd29f3994a40af
Jake Lundberg <allhailthewraith@protonmail.com>
2025-07-01 21:52:37 -0400

    :mushroom: issue/10 ui

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+        <h1 class="text-3xl font-bold underline">
+            Hello World
+        </h1>
+
+        <button class="btn btn-primary">
+            Click me! <Icon name="tabler:map" />
+        </button>
     </div>
 </template>
