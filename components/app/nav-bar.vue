b93c96e3f58ef149cb1660a9991b06829679e8c9
iamthe-Wraith <allhailthewraith@protonmail.com>
2025-07-14 21:35:35 -0400

    feat: :ghost: adds theme toggle to header

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -7,6 +7,8 @@
         </div>
 
         <div class="navbar-end">
+            <AppThemeToggle />
+
             <NuxtLink to="/" class="btn btn-accent text-xl">
                 Sign in with GitHub
                 <Icon name="tabler:brand-github" size="24" />
