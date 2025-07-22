ca97a3ee9a2bc503d9352e10c85924e2a5a9479d
wraith <wraith@wraiths-MacBook-Pro.local>
2025-07-22 07:32:11 -0400

    feat: adds auth store + error page

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -1,29 +1,15 @@
 <script setup lang="ts">
-import { createAuthClient } from 'better-auth/client';
-
-const authClient = createAuthClient();
-const loading = ref(false);
-
-async function signIn() {
-    loading.value = true;
-
-    await authClient.signIn.social({
-        provider: 'github',
-        callbackURL: 'http://localhost:3000/dashboard',
-    });
-
-    loading.value = false;
-}
+const authStore = useAuthStore();
 </script>
 
 <template>
     <button
-        :disabled="loading"
+        :disabled="authStore.loading"
         class="btn btn-accent text-xl"
-        @click="signIn"
+        @click="authStore.signIn"
     >
         Sign in with GitHub
-        <span v-if="loading" class="loading loading-spinner loading-md" />
+        <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
         <Icon v-else name="tabler:brand-github" size="24" />
     </button>
 </template>
