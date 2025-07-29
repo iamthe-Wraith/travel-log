a1fce04b0fdf184045c5e73114efa3cd32da338a
wraith <wraith@wraiths-MacBook-Pro.local>
2025-07-29 17:33:46 -0400

    feat: adds diff ui when user is authenticated

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -3,7 +3,27 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+    <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn m-1 gap-2">
+            <div class="avatar">
+                <div v-if="authStore.user.image" class="w-6 rounded-full">
+                    <img :src="authStore.user.image" :alt="authStore.user.name">
+                </div>
+            </div>
+
+            {{ authStore.user.name }}
+        </div>
+        <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+                <NuxtLink to="/sign-out">
+                    Sign Out
+                </NuxtLink>
+            </li>
+        </ul>
+    </div>
+
     <button
+        v-else
         :disabled="authStore.loading"
         class="btn btn-accent text-xl"
         @click="authStore.signIn"
---
+++ pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+    authStore.signOut();
+});
+</script>
+
+<template>
+    <div class="card container flex flex-col justify-center items-center gap-4 min-h-72 mt-4 mx-auto text-center bg-base-300">
+        <span class="loading loading-spinner loading-xl" />
+    </div>
+</template>
