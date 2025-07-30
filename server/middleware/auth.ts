081399c53f72e924ae0bc4cad34d37172fd18dd8
wraith <wraith@wraiths-MacBook-Pro.local>
2025-07-29 20:44:25 -0400

    feat: adds auth middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,10 @@
+import { auth } from '~/lib/auth';
+
+export default defineEventHandler(async (event) => {
+    if (event.path.startsWith('/dashboard')) {
+        const session = await auth.api.getSession(event);
+        if (!session) {
+            return sendRedirect(event, '/');
+        }
+    }
+});
